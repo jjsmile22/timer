f7a29562cd84e3057e7b0fa7f7a5fb15e6da6d45
Jordan Gertenstein <jordangert@hotmail.com>
2022-09-01 14:08:45 -0500

    refactored with Charlie to achieve edge cases

--- timer1.js
+++ timer1.js
@@ -1,8 +1,23 @@
 const myArgs = process.argv.slice(2);
 console.log('myArgs: ', myArgs);
 
-const fs = require('fs')
+const fs = require('fs');
 
-for (let i = 0; i < myArgs.length; i++) {
-  setTimeout(() => {  process.stdout.write('\x07')}, myArgs[i] * 1000)
+
+if (myArgs.length === 0) {
+  return;
 }
+
+const newArray = myArgs.reduce((previousValue, each) => {
+if((Number(each)) && (each > -1)) {
+  previousValue.push(each);
+ 
+ }
+ return previousValue;
+}, []);
+
+console.log(newArray);
+for (let i = 0; i < newArray.length; i++) {
+
+    setTimeout(() => { process.stdout.write('\x07'); }, newArray[i] * 1000);
+};
